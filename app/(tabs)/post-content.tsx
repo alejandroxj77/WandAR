c9898ba743db1083fe84e5e9a417935f3d2e4504
Manuel Alejandro <a.davilacisnero@gmail.com>
2025-12-31 14:21:48 -0600

    Supporing videos in AR

--- app/(tabs)/post-content.tsx
+++ app/(tabs)/post-content.tsx
@@ -1,15 +1,38 @@
-import { ViroARScene, ViroARSceneNavigator } from '@reactvision/react-viro';
+import { ViroARScene, ViroARSceneNavigator, ViroNode, ViroVideo } from '@reactvision/react-viro';
+import { View } from 'react-native';
 import ObjectPickerMenu from '../../presentation/molecules/ObjectPickerMenu';
 
 export default function PostContent() {
     return (
-        <>
+        <View style={{ flex: 1 }}>
             <ViroARSceneNavigator
-                initialScene={{ scene: () => <ViroARScene></ViroARScene> }}
+                initialScene={{
+                    scene: () => <ViroARScene>
+                        <ViroNode position={[0, 0, -3]}>
+                            {/* <Viro3DObject
+                                type="GLTF"
+                                source={require('../../assets/example_3d_models/shiba/scene.gltf')}
+                                position={[0, 0, -1]}
+                                materials={[
+                                    require('../../assets/example_3d_models/shiba/textures/default_baseColor.png')
+                                ]}
+                                resources={[
+                                    require('../../assets/example_3d_models/shiba/scene.bin')
+                                ]}
+                            /> */}
+                            <ViroVideo
+                                source={require('../../assets/example_videos/eyes_without_a_face.mp4')}
+                                loop={true}
+                                position={[0, 2, -5]}
+                                scale={[2, 2, 0]}
+                            />
+                        </ViroNode>
+                    </ViroARScene>
+                }}
                 style={{ flex: 1 }}
             >
             </ViroARSceneNavigator>
             <ObjectPickerMenu />
-        </>
+        </View>
     );
 }
--- presentation/molecules/ObjectPickerMenu.tsx
+++ presentation/molecules/ObjectPickerMenu.tsx
@@ -1,27 +1,28 @@
 import { BlurView } from "expo-blur";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import ShapesIcon from "../atoms/icons/ShapesIcon";
 
 export default function ObjectPickerMenu() {
-    return <View>
+    return (
         <Pressable style={styles.container}>
             <BlurView intensity={20} style={styles.blur} />
             <ShapesIcon />
-        </Pressable>
-    </View>
+        </Pressable>)
 }
 
 const styles = StyleSheet.create({
     container: {
         width: 45,
-        height: '80%',
+        height: '70%',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 22.5,
         overflow: 'hidden',
-        position: 'relative',
+        position: 'absolute',
         right: 16,
         marginVertical: 20,
+        bottom: 20,
+        top: 50,
     },
     blur: {
         ...StyleSheet.absoluteFillObject,
