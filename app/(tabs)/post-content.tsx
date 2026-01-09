ca0cbf33ab1f677c71205077db752892843d4e00
Samuel Mendoza <samuel@qordev.com>
2026-01-08 18:44:19 -0600

    revert post-content change

--- app/(tabs)/post-content.tsx
+++ app/(tabs)/post-content.tsx
@@ -1,12 +1,11 @@
-import { View } from "react-native";
+import { ViroARScene, ViroARSceneNavigator } from '@reactvision/react-viro';
 
 export default function PostContent() {
     return (
-        // <ViroARSceneNavigator
-        //     initialScene={{ scene: () => <ViroARScene /> }}
-        //     style={{ flex: 1 }}
-        // >
-        // </ViroARSceneNavigator>
-        <View/>
+        <ViroARSceneNavigator
+            initialScene={{ scene: () => <ViroARScene /> }}
+            style={{ flex: 1 }}
+        >
+        </ViroARSceneNavigator>
     );
 }
